e9f9d45c78eaad6c49878e7a7183b9e41fa23c92
harunlakodla <harunlakodla@gmail.com>
2022-11-16 01:02:42 +0300

    added model and xml_parser for http_get

--- lib/services/api/news_api.dart
+++ lib/services/api/news_api.dart
@@ -1,15 +1,19 @@
 
-import 'package:http/http.dart' ;
+import 'package:http/http.dart' as http;
+import 'package:xml_parser/xml_parser.dart' as xml_parser;
+import '../news_websites.dart';
 
 class NewsApi {
 
-  late Uri website_url;
-
+  late String website_url;
   NewsApi(){}
 
 
-  Future http_get(String website_url){
+  Future http_get(news_sitesVal website_url) async{
     
+    final my_val = await http.get(Uri.http(website_url.web_url.toString(), ""));
+    final hackerNews =
+        xml_parser.XmlDocument.from(my_val.toString(), parseCharacterEntities: false)!;
     return Future.value(-1);
   }
   
