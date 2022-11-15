51940c9af06542c8478abad6f17cc77da1c75db9
harunlakodla <harunlakodla@gmail.com>
2022-11-16 00:44:25 +0300

    update model and extension web_urls link

--- lib/services/news_websites.dart
+++ lib/services/news_websites.dart
@@ -17,6 +17,32 @@ enum news_sitesVal{
 }
 
 extension WebSiteSelection on news_sitesVal{
+
+  /***
+   * Burada yapman gereken şey senin tam olarak verileri button ile benim sevice sistemime göndermen
+   * gerektiğigidir.
+   * 
+   */
+
+  String? get web_url{
+    switch (this) {
+      case news_sitesVal.ekonomi:
+        return website_urls[0];
+        break;
+      case news_sitesVal.spor:
+        return website_urls[1];
+        break;
+      case news_sitesVal.teknoloji:
+        return website_urls[2];
+        break;
+      case news_sitesVal.kadin:
+        return website_urls[3];
+        break;
+      default:
+        
+    }
+  }
+
   // String web_url(){
   //   return website_urls[news_sitesVal.ekonomi]//** */
   // }
