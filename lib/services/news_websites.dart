29724a8b5f34161f4c908ac1d618f5a88226d456
harunlakodla <harunlakodla@gmail.com>
2022-11-15 17:20:59 +0300

    aa

--- lib/services/news_websites.dart
+++ lib/services/news_websites.dart
@@ -4,4 +4,12 @@ String haberturk_rss="https://www.haberturk.com/rss/anasayfa";
 
 List<String> website_urls=[
   "www.haberturk.com/rss/ekonomi.xml",
+  "www.haberturk.com/rss/spor.xml",
+  "www.haberturk.com/rss/kategori/teknoloji.xml",
+  "www.haberturk.com/rss/kategori/kadin.xml"
 ];
+
+Enum news_sitesVal{
+  ekonomi,
+
+}
