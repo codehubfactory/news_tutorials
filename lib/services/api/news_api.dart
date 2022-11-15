eb596ef8c706aa09093bb2802fb1b5c7157f39f3
harunlakodla <harunlakodla@gmail.com>
2022-11-15 17:06:14 +0300

    "added news_api.dart file"

---
+++ lib/services/api/news_api.dart
@@ -0,0 +1,18 @@
+
+import 'package:http/http.dart' ;
+
+class NewsApi {
+
+  late Uri website_url;
+
+  NewsApi(){}
+
+
+  Future http_get(String website_url){
+    
+    return Future.value(-1);
+  }
+  
+
+
+}
