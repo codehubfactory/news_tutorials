9f63b1da4d583b31885d1f3b885375a9316241ed
harunlakodla <harunlakodla@gmail.com>
2022-12-06 15:15:27 +0300

    changed files

--- lib/services/api/news_api.dart
+++ lib/services/api/news_api.dart
@@ -2,6 +2,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:webfeed/domain/rss_feed.dart';
 import 'package:xml2json/xml2json.dart';
 import 'package:xml_parser/xml_parser.dart' as xml_parser;
 import 'package:xml_parser/xml_parser.dart';
@@ -10,21 +11,25 @@ import '../news_websites.dart';
 class NewsApi {
 
   late String website_url;
+  final val =Xml2Json();
+
   NewsApi(){}
 
 
   Future http_get(news_sitesVal website_url) async{
     print(website_url.web_url.toString());
 
-    final my_val = await http.get(Uri.http(haberturk_rss.toString(), "/rss/ekonomi.xml"));
+    final my_val = await http.get(Uri.http(haberturk_rss.toString(),website_url.web_url.toString()));
     if (my_val is http.Response){
-      
-    final val =Xml2Json();
-    final hackerNews =
-        xml_parser.XmlDocument.from(my_val.body.toString(), parseCharacterEntities: false);
-        
-        val.parse(my_val.body.toString());
+      var valx=utf8.decode(my_val.bodyBytes);
+        print(valx);
+        var channel=RssFeed.parse(valx.toString());      
+        val.parse(valx.toString());
+        // jsonseriziable eklemek gerekli, daha pratik kullanım için...
         dynamic valjson=jsonDecode(val.toGData());
+
+        print(channel.items![0].title.toString());
+
         print(valjson["rss"]["channel"]["item"][0]["image"]["__cdata"]);
         print(valjson["rss"]["channel"]["item"][0]["description"]["__cdata"]);
 
@@ -41,6 +46,8 @@ class NewsApi {
 /**
  * 
  * old script
+ * final hackerNews =
+        xml_parser.XmlDocument.from(my_val.body.toString(), parseCharacterEntities: false);
  *   // final channel =  hackerNews?.getElement('channel')?.getChildren("item")?[0].getElement("title");
       final channel =  hackerNews?.getElement('channel')?.getChildren("item");
 
