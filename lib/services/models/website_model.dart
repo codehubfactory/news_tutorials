a0274688c7c4251d61cf42a342ed1e23e29b07de
harunlakodla <harunlakodla@gmail.com>
2022-12-07 23:17:51 +0300

    added website model for our systems

---
+++ lib/services/models/website_model.dart
@@ -0,0 +1,21 @@
+class WebSitePage {
+  String? websiteInfo;  
+  Channel? channel;
+}
+
+class Channel {
+  
+}
+
+class Items {
+  String? title;
+  String? link;
+  String? description;
+  String? category;
+  ImageWeb? imageWeb;
+
+}
+
+class ImageWeb{
+  String? image_url;
+}
